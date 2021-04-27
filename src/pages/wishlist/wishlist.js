d3835ab2562b8e7ae64b365d30bf6a5cc2be49e3
steve <stevo12424@live.com>
2021-04-26 23:27:15 -0700

    first commit

--- src/pages/wishlist/wishlist.js
+++ src/pages/wishlist/wishlist.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
 import {productcontext} from '../../data/productdata/product.context';
-
+import './wishlist.css';
 const Wishlist = ()=>
 {
     const wishlistitems = useContext(productcontext).wishlistitems;
-
+    
     return(
         <div className="wishlist-wrapper">
             {wishlistitems.map(items=>(
